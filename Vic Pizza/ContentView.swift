0969f2d46ccc9d4ad2dd10e9b105406284517e21
sunimalherath <sunimaldv@gmail.com>
2020-09-12 16:41:26 +1000

    Initial Commit

---
+++ Vic Pizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vic Pizza
+//
+//  Created by Sunimal Herath on 12/9/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
