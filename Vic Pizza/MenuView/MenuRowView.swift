eb5ce2cbc8968006417ce6dece3261c2b2239cc2
sunimalherath <sunimaldv@gmail.com>
2020-10-07 18:50:24 +1100

    adding styles to pizza images

--- Vic Pizza/MenuView/MenuRowView.swift
+++ Vic Pizza/MenuView/MenuRowView.swift
@@ -13,6 +13,10 @@ struct MenuRowView: View {
         List(0..<5) { item in
             HStack(alignment: .top, spacing: 15) {
                 Image("1_100w")
+                    .cornerRadius(10)
+                    //.border(Color("G4"),width: 2)
+                    .clipShape(Capsule())
+                    .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 0)
                 VStack(alignment:.leading, spacing: 5) {
                     Text("Vic Chicken Pizza")
                         .font(.headline)
