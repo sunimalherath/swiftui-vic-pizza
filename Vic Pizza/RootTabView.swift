ce3256b86e48978b91de66fff47218a920a7bf86
sunimalherath <sunimaldv@gmail.com>
2020-10-15 18:06:45 +1100

    adding tab bar

---
+++ Vic Pizza/RootTabView.swift
@@ -0,0 +1,33 @@
+//
+//  RootTabView.swift
+//  Vic Pizza
+//
+//  Created by Sunimal Herath on 15/10/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        TabView{
+            ContentView(orderModel: OrderModel())
+                .tabItem{
+                    Image(systemName: "cart")
+                    Text("Order")
+                }
+            HistoryView()
+                .tabItem {
+                    Image(systemName: "book")
+                    Text("History")
+                }
+        }
+        .accentColor(Color("B1"))
+    }
+}
+
+struct RootTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootTabView().environmentObject(UserPreferences())
+    }
+}
